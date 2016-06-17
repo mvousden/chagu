1365a3422e20ebf3a708ad00ab7f478550f77347
mvousden <mark.vousden@soton.ac.uk>
2016-06-17 14:23:44 +0100

    FEAT-002: Initial commit of termini test script. Currently only testing nasty_arrow_polydata.

---
+++ test/test_termini.py
@@ -0,0 +1,54 @@
+"""
+This python file tests the functionality of functions defined in
+chagu/termini.py. Tests are detailed in the function documentation.
+"""
+
+import chagu
+import pytest
+import vtk
+
+
+def test_nasty_arrow_polydata()
+    """
+    Test chagu.mask.create_mask_from_opts. We test the following cases:
+
+    1. If any dimension is not positive, a ValueError is raised.
+    2. If all is well, check the number of points and their positions for a few
+        configurations.
+    """
+
+    # Test 1: If any dimension is not positive, a ValueError is raised
+    with pytest.raises(ValueError) as testException:
+        chagu.termini.nasty_arrow_polydata(-14.34, 10, 2)
+    assert "positive" in testException.value.message
+    assert "-14.34" in testException.value.message
+
+    with pytest.raises(ValueError) as testException:
+        chagu.termini.nasty_arrow_polydata(14, 7.5, 2)
+    assert "positive" in testException.value.message
+    assert "7.5" in testException.value.message
+
+    with pytest.raises(ValueError) as testException:
+        chagu.termini.nasty_arrow_polydata(14, 7, 0)
+    assert "positive" in testException.value.message
+    assert "0" in testException.value.message
+
+    # Test 2: If all is well, check the number of points and their positions
+    # for a few configurations.
+    lx = 10
+    ly = 5
+    lz = 2
+    arrowPolyData = chagu.termini.nasty_arrow_polydata(lx, ly, lz
+
+    assert arrowPolyData.GetNumberOfPoints() == 18
+    assert np.abs(np.array(arrowPolyData.GetPoint(0)) -
+                  np.array([-lx / 2., lz / 2., lz / 2.])) < 1e-3
+    assert np.abs(np.array(arrowPolyData.GetPoint(4)) -
+                  np.array([lx / 2., 0., lz / 2.])) < 1e-3
+
+    # I could easily waste all day checking each point meticulously, but a
+    # visual check would save so much time if this is changed in future.
+
+
+if __name__ == "__main__":
+    test_nasty_arrow_polydata()
